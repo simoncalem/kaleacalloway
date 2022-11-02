87c82a45fc45bf4691c8e4b42be794c1437d328a
Simon <simonnikocalem@gmail.com>
2022-11-01 21:26:16 -0700

    fixed smh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
-import { Analytics } from '@vercel/analytics/react'
+import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
