6488e469b361faafa8bd4c4b13711a38c99f2f24
Simon <simonnikocalem@gmail.com>
2022-12-30 13:12:29 -0800

    new font

--- components/posts-grid.tsx
+++ components/posts-grid.tsx
@@ -4,8 +4,8 @@ import PostPreview from './post-preview'
 export default function PostsGrid({ posts }: { posts: PostProps[] }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl"></h2>
-      <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-2 md:gap-x-16 md:gap-y-32 lg:gap-x-32">
+      <h2 className="mb-8 text-6xl md:text-7xl"></h2>
+      <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-3 md:gap-x-8 md:gap-y-32 lg:gap-x-8">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/index.css'
 
+// pages/_app.js
+import { Xanh_Mono } from '@next/font/google'
 import { Analytics } from '@vercel/analytics/react'
-
+const xanh = Xanh_Mono({ weight: ['400'] })
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
-      <Analytics />
+      <main className={xanh.className}>
+        <Component {...pageProps} />
+        <Analytics />{' '}
+      </main>
     </>
   )
 }
